7540655e1445607e48951b6e133eeb11019fdafd
Maciej Wiatr <maciej.wiatr@microsoft.wsei.edu.pl>
2023-10-28 10:23:04 +0200

    Add customers controller & view

---
+++ Lab2AD/Lab2AD/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using LibApp.Models;
+using LibApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var viewModel = new IndexCustomersViewModel
+            {
+                Customers = GetCustomers()
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().Single(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "Stefan batory"},
+                new Customer { Id = 2, Name = "Stefan dwatory"},
+                new Customer { Id = 3, Name = "Stefan trzytory"}
+            };
+
+            return customers;
+        }
+    }
+}
---
+++ Lab2AD/Lab2AD/ViewModels/IndexCustomersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LibApp.Models;
+
+namespace LibApp.ViewModels
+{
+    public class IndexCustomersViewModel
+    {
+        public IEnumerable<Customer> Customers { get; set; }
+
+        public IndexCustomersViewModel()
+        {
+
+        }
+    }
+}
